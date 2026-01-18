7a9d8d5caebdc2df192a9a1b3fe23eaa00176f24
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-18 21:50:27 +0600

    feat: add user model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, trim: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "customer"], default: "customer" },
+  },
+  { timestamps: true },
+);
