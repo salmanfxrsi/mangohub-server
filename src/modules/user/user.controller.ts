e9704b25b68077eb810f475dd1a8ac93bef9b18a
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-18 23:23:38 +0600

    feat: add RegisterUser endpoint

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import User from "./user.model";
+
+export const registerUser = async (req: Request, res: Response) => {
+  const payload = req.body;
+  const user = new User(payload);
+  const data = await user.save;
+
+  res.send({
+    success: true,
+    message: "User Registered Successfully",
+    data,
+  });
+};
--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,13 +1,22 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IUser } from "./user.interface";
 
 const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
     phone: { type: String, required: true, trim: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["admin", "customer"], default: "customer" },
   },
   { timestamps: true },
 );
+
+const User = model<IUser>("User", userSchema);
+export default User;
